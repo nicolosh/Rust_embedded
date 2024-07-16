c654b77954efa25d0d84ce02969a6b838d162c90
senyo simpson <simpsonsenyo@gmail.com>
2024-07-16 22:59:49 +0200

    up to date again

--- registers/src/main.rs
+++ registers/src/main.rs
@@ -3,16 +3,39 @@
 
 use core::panic::PanicInfo;
 
+use cortex_m::asm;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
+use cortex_m_rt::exception;
 use cortex_m_semihosting::hprintln;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-  loop {}
+    loop {}
 }
+
+// CPU frequency (12.5 MHz by default)
+const CPU_FREQ: u32 = 12_500_000;
 
 #[entry]
 fn main() -> ! {
-  hprintln!("Starting program!");
-  loop {}
+    hprintln!("Starting program!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.enable_interrupt();
+    systick.set_clock_source(SystClkSource::Core);
+    systick.set_reload(CPU_FREQ);
+    systick.clear_current();
+    systick.enable_counter();
+
+    loop {
+        asm::wfi();
+    }
+}
+
+#[exception]
+fn SysTick() {
+    hprintln!("ugh, woke up :(")
 }
