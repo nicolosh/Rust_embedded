4322dbe9fa942de1af6ab9e586e4b6c64b910765
senyo simpson <simpsonsenyo@gmail.com>
2024-07-16 23:12:10 +0200

    up to date

--- timers/src/main.rs
+++ timers/src/main.rs
@@ -15,28 +15,23 @@ fn panic(_info: &PanicInfo) -> ! {
 
 // Run-Mode Clock Configuration (RCC)
 const RCC: u32 = 0x400FE060;
-// Prescaler values
-const SYSCTL_SYSDIV_16: u32 = 16; // 12.5 MHz
-const SYSCTL_SYSDIV_12: u32 = 12; // 16.67 MHz
+// Prescaler value
+const SYSCTL_SYSDIV_16: u32 = 0xF; // 12.5 MHz
+const SYSCTL_SYSDIV_12: u32 = 0xB; // 16.67 MHz
+
+// CPU frequency (12.5 MHz by default)
+const CPU_FREQ: u32 = 12_500_000;
 
 #[entry]
 fn main() -> ! {
     hprintln!("Starting program!");
 
-    let div = SYSCTL_SYSDIV_12;
-    let freq = match div {
-        SYSCTL_SYSDIV_12 => 16_670_000,
-        SYSCTL_SYSDIV_16 => 12_500_000,
-        _ => 12_500_000,
-    };
-
-    // Set the RCC value
+    // Set the prescaler value
     unsafe {
-        let sysdiv = div << 23;
-        let rcc = {
-            let orig = *(RCC as *const u32);
-            orig | sysdiv
-        };
+        let sysdiv = SYSCTL_SYSDIV_16 << 23;
+        let orig = *(RCC as *const u32);
+        let mask = !0b1111 << 23;
+        let rcc = (orig & mask) | sysdiv;
         *(RCC as *mut u32) = rcc;
     };
 
@@ -44,7 +39,7 @@ fn main() -> ! {
     let mut systick = core_peripherals.SYST;
     systick.enable_interrupt();
     systick.set_clock_source(SystClkSource::Core);
-    systick.set_reload(freq);
+    systick.set_reload(CPU_FREQ);
     systick.clear_current();
     systick.enable_counter();
 
